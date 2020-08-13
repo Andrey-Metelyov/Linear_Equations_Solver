013c780b84d8e7ac3019037f4c3a05a886359b84
Andrey.Metelyov <andrey.metelyov@gmail.com>
2020-08-13 12:52:20 +0900

    2/5

--- Linear Equations Solver/task/src/solver/Main.java
+++ Linear Equations Solver/task/src/solver/Main.java
@@ -7,8 +7,14 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
-        // a * x = b
-        // x = b / a;
-        System.out.println(b / a);
+        double c = scanner.nextDouble();
+        double d = scanner.nextDouble();
+        double e = scanner.nextDouble();
+        double f = scanner.nextDouble();
+        // a * x + b * y = c
+        // d * x + e * y = f;
+        double y = (f - c * d / a) / (e - b * d / a);
+        double x = (c - b * y) / a;
+        System.out.println(x + " " + y);
     }
 }
--- Linear Equations Solver/task/test/SolverTest.java
+++ Linear Equations Solver/task/test/SolverTest.java
@@ -1,32 +1,35 @@
 import org.hyperskill.hstest.testcase.CheckResult;
 import org.hyperskill.hstest.stage.StageTest;
 import org.hyperskill.hstest.testcase.TestCase;
-
 import solver.Main;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 import static java.lang.Math.abs;
 
 
 public class SolverTest extends StageTest<String> {
 
-    public SolverTest() {
+    public SolverTest() throws Exception {
         super(Main.class);
     }
 
     @Override
     public List<TestCase<String>> generate() {
         List<TestCase<String>> tests = List.of(
-            new TestCase<String>().setInput("5 3"),
-            new TestCase<String>().setInput("12 67"),
-            new TestCase<String>().setInput("12 12"),
-            new TestCase<String>().setInput("-1 1"),
-            new TestCase<String>().setInput("2 -3"),
-            new TestCase<String>().setInput("2.34 12.23")
+            new TestCase<String>()
+                .setInput("4 5 7\n3 9 9"),
+            new TestCase<String>()
+                .setInput("1 2 3\n4 5 6"),
+            new TestCase<String>()
+                .setInput("1.2 3.4 5.6\n7.8 9.0 12.3"),
+            new TestCase<String>()
+                .setInput("23.21 32.12 65.43\n1 0 1")
         );
 
-        for (TestCase<String> test : tests) {
+        for (var test : tests) {
             test.setAttach(solve(test.getInput()));
         }
 
@@ -34,18 +37,35 @@ public class SolverTest extends StageTest<String> {
     }
 
     private String solve(String input) {
-        String[] nums = input.split(" ");
-        double a = Double.parseDouble(nums[0]);
-        double b = Double.parseDouble(nums[1]);
-        return String.valueOf(b / a);
+        Scanner sc = new Scanner(input);
+
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+        double d = sc.nextDouble();
+        double e = sc.nextDouble();
+        double f = sc.nextDouble();
+
+        double y = (f - c * (d / a)) / (e - b * (d / a));
+        double x = (c - b * y) / a;
+
+        return x + " " + y;
     }
 
     @Override
     public CheckResult check(String reply, String clue) {
         try {
-            double actual = Double.parseDouble(reply);
-            double expected = Double.parseDouble(clue);
-            return new CheckResult(abs(actual - expected) < 0.001);
+            double[] actual = Arrays.stream(reply.split(" ")).mapToDouble(Double::parseDouble).toArray();
+            double[] expected = Arrays.stream(clue.split(" ")).mapToDouble(Double::parseDouble).toArray();
+            if (actual.length != expected.length) {
+                return CheckResult.wrong("");
+            }
+            for (int i = 0; i < actual.length; i++) {
+                if (abs(actual[i] - expected[i]) > 0.001) {
+                    return CheckResult.wrong("");
+                }
+            }
+            return CheckResult.correct();
         }
         catch (NumberFormatException ex) {
             return new CheckResult(false, "Can't parse numbers - check if you don't print any additional symbols except a space between the numbers");
